1bf8516a6be553b27229aabd84ae70c4ecbe4f1d
Jack Coble <jack@coble.uk>
2024-03-02 13:01:20 +0000

    When authenticating, store access token within the Client

--- cmd/puregym/main.go
+++ cmd/puregym/main.go
@@ -15,37 +15,40 @@ const (
 
 // Client represents the PureGym API Client
 type Client struct {
-	Email string
-	PIN   string
+	email string
+	pin   string
 
-	AccessToken string
+	accessToken string
 }
 
 // Return a new Client
 func NewClient(email string, pin string) *Client {
 	return &Client{
-		Email: email,
-		PIN:   pin,
+		email: email,
+		pin:   pin,
 	}
 }
 
 // Authenticate against the PureGym API to return an Access Token
-func (c *Client) Authenticate() (*types.AuthenticateResponse, error) {
+func (c *Client) Authenticate() error {
 	// Construct the URL Encoded Form request
 	requestBody := types.AuthenticateRequest{
-		Username:  c.Email,
-		Password:  c.Email,
+		Username:  c.email,
+		Password:  c.pin,
 		GrantType: "password",
 		Scope:     "pgcapi",
 		ClientID:  "ro.client",
 	}
 
-	vals, _ := query.Values(requestBody)
+	vals, err := query.Values(requestBody)
+	if err != nil {
+		return err
+	}
 
 	// Create the HTTP POST request
 	req, err := http.NewRequest("POST", AUTH_URL, bytes.NewBuffer([]byte(vals.Encode())))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	// Set the Request headers
@@ -56,15 +59,18 @@ func (c *Client) Authenticate() (*types.AuthenticateResponse, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 
 	// Marshal response into JSON
 	var authResponse types.AuthenticateResponse
 	if err := json.NewDecoder(resp.Body).Decode(&authResponse); err != nil {
-		return nil, err
+		return err
 	}
 
-	return &authResponse, nil
+	// Extract and set the Access Token for future requests
+	c.accessToken = authResponse.AccessToken
+
+	return nil
 }
