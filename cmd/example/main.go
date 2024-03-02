26050333dceed4ad24ac6438ad5e215234dde75c
Jack Coble <jack@coble.uk>
2024-03-02 14:26:12 +0000

    Add an example program of how to use the package

---
+++ cmd/example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/jackcoble/puregym-go/cmd/puregym"
+)
+
+func main() {
+	// Create new instance of the PureGym API Client
+	pureGym := puregym.NewClient(os.Getenv("PUREGYM_EMAIL"), os.Getenv("PUREGYM_PIN"))
+
+	// Authenticate against the API
+	if err := pureGym.Authenticate(); err != nil {
+		log.Fatalf("unable to authenticate: %s", err.Error())
+	}
+
+	// Set the Home Gym for the client to use
+	if err := pureGym.SetHomeGym(); err != nil {
+		log.Printf("unable to set home gym for client: %s", err.Error())
+	}
+
+	// Get Live Capacity at Gym
+	liveCapacity, err := pureGym.GetLiveCapacity()
+	if err != nil {
+		log.Printf("unable to fetch live capacity: %s", err.Error())
+	}
+
+	log.Printf("Total number of people inside the Gym: %d", liveCapacity)
+}
